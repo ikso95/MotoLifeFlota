b8b88943ecddfc424ca1fdcebefc249ddace1839
Oskar <oskail@wp.pl>
2019-12-10 14:24:10 +0100

    Implemented password visibility button in firstRunApp Activity

--- app/src/main/java/com/example/motolifeflota/FirstRunActivity.java
+++ app/src/main/java/com/example/motolifeflota/FirstRunActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +15,10 @@ import android.widget.EditText;
 public class FirstRunActivity extends AppCompatActivity {
 
     private EditText password;
-    private Button checkPassword;
+    private Button checkPassword, password_visibility;
     private SharedPreferences sharedPreferences;
+
+    private boolean isPasswordVisible = false;
 
     private final static String appPassword="Haslo";
 
@@ -25,6 +29,7 @@ public class FirstRunActivity extends AppCompatActivity {
 
         password=findViewById(R.id.app_password_EditText);
         checkPassword=findViewById(R.id.checkPassword_Button);
+        password_visibility=findViewById(R.id.password_visibility_button);
 
         sharedPreferences = getApplicationContext().getSharedPreferences("Password", Context.MODE_PRIVATE); // 0 - for private mode
 
@@ -34,6 +39,33 @@ public class FirstRunActivity extends AppCompatActivity {
             startActivity(goToMainActivityIntent);
             finish();
         }
+
+
+        password_visibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isPasswordVisible ==false)
+                {
+                    isPasswordVisible = true;
+                    password_visibility.setBackground(getResources().getDrawable(R.drawable.ic_visibility));
+                    password.setInputType(InputType.TYPE_CLASS_TEXT);
+                    password.setTransformationMethod(null);
+                    password.setSelection(password.getText().length()); //ustawienie kursora na koniec tekstu przy zmianie widoczności wracał na początek
+                }
+                else
+                {
+                    isPasswordVisible=false;
+                    password_visibility.setBackground(getResources().getDrawable(R.drawable.ic_visibility_off));
+                    password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    password.setSelection(password.getText().length()); //ustawienie kursora na koniec tekstu przy zmianie widoczności wracał na początek
+
+                }
+
+
+            }
+        });
+
 
         checkPassword.setOnClickListener(new View.OnClickListener() {
             @Override
